31e79b74dd14cfbcd7ea9894e9db76c8cb5bea2b
Dvstrada <jjes02233@gmail.com>
2025-08-21 21:11:55 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Dvstrada'
+description 'Control engine toggle with Shift and interactive door/lights UI with U key'
+version '1.0.0'
+
+lua54 'yes'
+
+client_scripts {
+    'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
